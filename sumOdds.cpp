da7d39724b9d8110b74fc32e7296bf8d8bcc72df
Regina <miao92@ucsb.edu>
2024-02-07 12:33:15 -0800

    message

--- sumOdds.cpp
+++ sumOdds.cpp
@@ -5,5 +5,11 @@
 #include "utility.h"
 
 int sumOdds(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  // STUB!  Replace with correct code.
+  int sum = 0; // Initialize sum to 0
+  for (int i = 0; i < size; i++) { 
+    if (isOdd(a[i])) { // If a[i] is odd
+      sum += a[i];
+    }
+  }
 }
