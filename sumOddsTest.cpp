d691640c7dedb6eecd5b4e83e41e943cfbf2c27f
Regina <miao92@ucsb.edu>
2024-02-07 17:37:10 -0800

    message

--- sumOddsTest.cpp
+++ sumOddsTest.cpp
@@ -7,39 +7,23 @@ int main() {
   // code for sumOdds() correctly sums up all the odd numbers in
   // arrays of various sizes.  Include both positive, negative and
   // zero values in your test arrays.
-  int fiveThrees[5]={3,3,3,3,3};
-  assertEquals(15,
-	       sumOdds(fiveThrees,5),
-	       "sumOdds(fiveThrees,5)");
+
+  int fiveThrees[5] = {3, 3, 3, 3, 3};
+  assertEquals(15, sumOdds(fiveThrees, 5), "sumOdds(fiveThrees,5)");
   
-  int zeros[3]={0,0,0}; // initializes all elements to 0
-  assertEquals(0,
-	       sumOdds(zeros,3), 
-	       "sumOdds(zeros,3)" );  
+  int zeros[3] = {0, 0, 0};
+  assertEquals(0, sumOdds(zeros, 3), "sumOdds(zeros,3)");  
 
-  
-  int fiveInts[5]={12,3,4,-9,1}; 
-  assertEquals(11,
-	       sumOdds(fiveInts,5), 
-	       "sumOdds(fiveInts,5)" ); 
-
-  assertEquals(19,
-	       sumOdds(fiveInts,3), 
-	       "sumOdds(fiveInts,3)" ); 
-
-  assertEquals(15,
-	       sumOdds(fiveInts,2), 
-	       "sumOdds(fiveInts,2)" ); 
+  int fiveInts[5] = {12, 3, 4, -9, 1};
+  assertEquals(-5, sumOdds(fiveInts, 5), "sumOdds(fiveInts,5)");
+  assertEquals(3, sumOdds(fiveInts, 3), "sumOdds(fiveInts,3)");
+  assertEquals(3, sumOdds(fiveInts, 2), "sumOdds(fiveInts,2)");
 
   int meaning[] = {42};
-  assertEquals(42,
-	       sumOdds(meaning,1), 
-	       "sumOdds(meaning,1)" ); 
+  assertEquals(0, sumOdds(meaning, 1), "sumOdds(meaning,1)");
 
-  int mix[10]={-42,12,-9,56,123,-32767,48}; // last three will be zero
-  assertEquals(-32579,
-	       sumOdds(mix,10), 
-	       "sumOdds(mix,10)" );
+  int mix[10] = {-42, 12, -9, 56, 123, -32767, 48};
+  assertEquals(-32653, sumOdds(mix, 10), "sumOdds(mix,10)");
 
   return 0;
 }
